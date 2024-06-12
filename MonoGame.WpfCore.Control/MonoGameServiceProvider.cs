7300f0e6a01e4c299f4d06833c03c94cd081f3db
Dean Lunz <createdbyx@createdbyx.com>
2024-06-12 00:07:07 -0700

    Add MonoGame content control and related classes
    Added MonoGame content control along with related view model and service provider classes necessary for graphics device maintenance. Also updated project files: references were sorted, new packages were included, and application now targets .NET 6.0. Changes were also reflected in the solution file to include new C# project.

---
+++ MonoGame.WpfCore.Control/MonoGameServiceProvider.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MonoGame.WpfCore.MonoGameControls;
+
+public class MonoGameServiceProvider : IServiceProvider
+{
+    private readonly Dictionary<Type, object> _services;
+
+    public MonoGameServiceProvider()
+    {
+        this._services = new Dictionary<Type, object>();
+    }
+
+    public void AddService(Type type, object provider)
+    {
+        this._services.Add(type, provider);
+    }
+
+    public object GetService(Type type)
+    {
+        if (this._services.TryGetValue(type, out var service))
+            return service;
+
+        return null;
+    }
+
+    public void RemoveService(Type type)
+    {
+        this._services.Remove(type);
+    }
+
+    public void AddService<T>(T service)
+    {
+        this.AddService(typeof(T), service);
+    }
+
+    public T GetService<T>() where T : class
+    {
+        var service = this.GetService(typeof(T));
+        return (T) service;
+    }
+}
---
+++ MonoGame.WpfCore.Control/MonoGameViewModel.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MonoGame.WpfCore.MonoGameControls;
+
+public class MonoGameViewModel : INotifyPropertyChanged, IMonoGameViewModel
+{
+    public MonoGameViewModel()
+    {
+    }
+
+    public void Dispose()
+    {
+        this.Content?.Dispose();
+    }
+
+    private IGraphicsDeviceService graphicsDeviceService;
+
+    public IGraphicsDeviceService GraphicsDeviceService
+    {
+        get => this.graphicsDeviceService;
+        set => this.SetField(ref this.graphicsDeviceService, value);
+    }
+    
+    protected GraphicsDevice GraphicsDevice => this.GraphicsDeviceService?.GraphicsDevice;
+    protected MonoGameServiceProvider Services { get; private set; }
+    protected ContentManager Content { get; set; }
+
+    public virtual void Initialize()
+    {
+        this.Services = new MonoGameServiceProvider();
+        this.Services.AddService(this.GraphicsDeviceService);
+        this.Content = new ContentManager(this.Services) { RootDirectory = "Content" };
+    }
+
+    public virtual void LoadContent()
+    {
+    }
+
+    public virtual void UnloadContent()
+    {
+    }
+
+    public virtual void Update(GameTime gameTime)
+    {
+    }
+
+    public virtual void Draw(GameTime gameTime)
+    {
+    }
+
+    public virtual void OnActivated(object sender, EventArgs args)
+    {
+    }
+
+    public virtual void OnDeactivated(object sender, EventArgs args)
+    {
+    }
+
+    public virtual void OnExiting(object sender, EventArgs args)
+    {
+    }
+
+    public virtual void SizeChanged(object sender, SizeChangedEventArgs args)
+    {
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        this.OnPropertyChanged(propertyName);
+        return true;
+    }
+}
