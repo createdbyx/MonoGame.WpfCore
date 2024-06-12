30987d9e0fc02c05606604cb4c1af627b20aa080
Dean Lunz <createdbyx@createdbyx.com>
2024-06-12 00:48:04 -0700

    Refactor MonoGameGraphicsDeviceService for clarity
    The MonoGameGraphicsDeviceService code was refactored to improve code readability and maintainability. This includes making better use of private properties, reducing unnecessary public exposure, and simplifying the way some attributes are used. Extra white space and comments were also cleaned up to improve clarity.

--- MonoGame.WpfCore.Control/MonoGameGraphicsDeviceService.cs
+++ MonoGame.WpfCore.Control/MonoGameGraphicsDeviceService.cs
@@ -3,64 +3,61 @@ using System.Windows;
 using System.Windows.Interop;
 using Microsoft.Xna.Framework.Graphics;
 using SharpDX.Direct3D9;
+using PresentInterval = SharpDX.Direct3D9.PresentInterval;
 
 namespace MonoGame.WpfCore.MonoGameControls;
 
 public class MonoGameGraphicsDeviceService : IGraphicsDeviceService, IDisposable
 {
-    public MonoGameGraphicsDeviceService()
-    {
-    }
-
-    public void Dispose()
-    {
-        DeviceDisposing?.Invoke(this, EventArgs.Empty);
-        GraphicsDevice.Dispose();
-        Direct3DDevice?.Dispose();
-        Direct3DContext?.Dispose();
-    }
+    // Store the current device settings.
+    private PresentationParameters _parameters;
 
     public Direct3DEx Direct3DContext { get; private set; }
     public DeviceEx Direct3DDevice { get; private set; }
+
+    public void Dispose()
+    {
+        this.DeviceDisposing?.Invoke(this, EventArgs.Empty);
+        this.GraphicsDevice.Dispose();
+        this.Direct3DDevice?.Dispose();
+        this.Direct3DContext?.Dispose();
+    }
 
     public event EventHandler<EventArgs> DeviceCreated;
     public event EventHandler<EventArgs> DeviceDisposing;
     public event EventHandler<EventArgs> DeviceReset;
     public event EventHandler<EventArgs> DeviceResetting;
 
+    public GraphicsDevice GraphicsDevice { get; private set; }
+
     public void StartDirect3D(Window window)
     {
-        Direct3DContext = new Direct3DEx();
+        this.Direct3DContext = new Direct3DEx();
 
         var presentParameters = new PresentParameters
         {
             Windowed = true,
             SwapEffect = SwapEffect.Discard,
             DeviceWindowHandle = new WindowInteropHelper(window).Handle,
-            PresentationInterval = SharpDX.Direct3D9.PresentInterval.Default
+            PresentationInterval = PresentInterval.Default
         };
 
-        Direct3DDevice = new DeviceEx(Direct3DContext, 0, DeviceType.Hardware, IntPtr.Zero,
-                                      CreateFlags.HardwareVertexProcessing | CreateFlags.Multithreaded | CreateFlags.FpuPreserve,
-                                      presentParameters);
+        this.Direct3DDevice = new DeviceEx(this.Direct3DContext, 0, DeviceType.Hardware, IntPtr.Zero,
+                                           CreateFlags.HardwareVertexProcessing | CreateFlags.Multithreaded | CreateFlags.FpuPreserve,
+                                           presentParameters);
 
         // Create the device using the main window handle, and a placeholder size (1,1).
         // The actual size doesn't matter because whenever we render using this GraphicsDevice,
         // we will make sure the back buffer is large enough for the window we're rendering into.
         // Also, the handle doesn't matter because we call GraphicsDevice.Present(...) with the
         // actual window handle to render into.
-        GraphicsDevice = CreateGraphicsDevice(new WindowInteropHelper(window).Handle, 1, 1);
-        DeviceCreated?.Invoke(this, EventArgs.Empty);
+        this.GraphicsDevice = this.CreateGraphicsDevice(new WindowInteropHelper(window).Handle, 1, 1);
+        this.DeviceCreated?.Invoke(this, EventArgs.Empty);
     }
-
-    // Store the current device settings.
-    private PresentationParameters _parameters;
-
-    public GraphicsDevice GraphicsDevice { get; private set; }
 
     public GraphicsDevice CreateGraphicsDevice(IntPtr windowHandle, int width, int height)
     {
-        _parameters = new PresentationParameters
+        this._parameters = new PresentationParameters
         {
             BackBufferWidth = Math.Max(width, 1),
             BackBufferHeight = Math.Max(height, 1),
@@ -71,29 +68,29 @@ public class MonoGameGraphicsDeviceService : IGraphicsDeviceService, IDisposable
             IsFullScreen = false
         };
 
-        return new GraphicsDevice(GraphicsAdapter.DefaultAdapter, GraphicsProfile.HiDef, _parameters);
+        return new GraphicsDevice(GraphicsAdapter.DefaultAdapter, GraphicsProfile.HiDef, this._parameters);
     }
 
     /// <summary>
-    /// Resets the graphics device to whichever is bigger out of the specified
-    /// resolution or its current size. This behavior means the device will
-    /// demand-grow to the largest of all its GraphicsDeviceControl clients.
+    ///     Resets the graphics device to whichever is bigger out of the specified
+    ///     resolution or its current size. This behavior means the device will
+    ///     demand-grow to the largest of all its GraphicsDeviceControl clients.
     /// </summary>
     public void ResetDevice(int width, int height)
     {
-        var newWidth = Math.Max(_parameters.BackBufferWidth, width);
-        var newHeight = Math.Max(_parameters.BackBufferHeight, height);
+        var newWidth = Math.Max(this._parameters.BackBufferWidth, width);
+        var newHeight = Math.Max(this._parameters.BackBufferHeight, height);
 
-        if (newWidth != _parameters.BackBufferWidth || newHeight != _parameters.BackBufferHeight)
+        if (newWidth != this._parameters.BackBufferWidth || newHeight != this._parameters.BackBufferHeight)
         {
-            DeviceResetting?.Invoke(this, EventArgs.Empty);
+            this.DeviceResetting?.Invoke(this, EventArgs.Empty);
 
-            _parameters.BackBufferWidth = newWidth;
-            _parameters.BackBufferHeight = newHeight;
+            this._parameters.BackBufferWidth = newWidth;
+            this._parameters.BackBufferHeight = newHeight;
 
-            GraphicsDevice.Reset(_parameters);
+            this.GraphicsDevice.Reset(this._parameters);
 
-            DeviceReset?.Invoke(this, EventArgs.Empty);
+            this.DeviceReset?.Invoke(this, EventArgs.Empty);
         }
     }
 }
