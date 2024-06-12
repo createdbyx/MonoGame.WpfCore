993283431cb0d060853eb70377e47d6b3caed881
Dean Lunz <createdbyx@createdbyx.com>
2024-06-12 00:40:12 -0700

    Refactor MonoGameViewModel and improve documentation
    The code refactors MonoGameViewModel and it provides detailed inline documentation for better understanding and maintenance. The refactor focuses on GraphicsDeviceService and PropertyChanged functionalities making the code more readable and organized. Furthermore, detailed comments are added explaining the purpose and the usage of the important methods and properties.

--- MonoGame.WpfCore.Control/MonoGameViewModel.cs
+++ MonoGame.WpfCore.Control/MonoGameViewModel.cs
@@ -11,16 +11,29 @@ namespace MonoGame.WpfCore.MonoGameControls;
 
 public class MonoGameViewModel : INotifyPropertyChanged, IMonoGameViewModel
 {
-    public MonoGameViewModel()
+    private IGraphicsDeviceService graphicsDeviceService;
+
+    protected GraphicsDevice GraphicsDevice
     {
+        get
+        {
+            return this.GraphicsDeviceService?.GraphicsDevice;
+        }
     }
+
+    /// <summary>
+    /// Gets or sets an instance of <see cref="MonoGameServiceProvider"/>.
+    /// This provides functionalities to implement dependency injection in MonoGame applications.
+    /// This property is set during the initialization of the game.
+    /// </summary>
+    protected MonoGameServiceProvider Services { get; private set; }
+
+    protected ContentManager Content { get; set; }
 
     public void Dispose()
     {
         this.Content?.Dispose();
     }
-
-    private IGraphicsDeviceService graphicsDeviceService;
 
     public IGraphicsDeviceService GraphicsDeviceService
     {
@@ -28,10 +41,6 @@ public class MonoGameViewModel : INotifyPropertyChanged, IMonoGameViewModel
         set => this.SetField(ref this.graphicsDeviceService, value);
     }
     
-    protected GraphicsDevice GraphicsDevice => this.GraphicsDeviceService?.GraphicsDevice;
-    protected MonoGameServiceProvider Services { get; private set; }
-    protected ContentManager Content { get; set; }
-
     public virtual void Initialize()
     {
         this.Services = new MonoGameServiceProvider();
@@ -71,16 +80,66 @@ public class MonoGameViewModel : INotifyPropertyChanged, IMonoGameViewModel
     {
     }
 
+    /// <summary>
+    /// Occurs when a property value changes.
+    /// </summary>
+    /// <remarks>
+    /// This event is meant to be used with the INotifyPropertyChanged interface 
+    /// to signal the fact that a property has changed, and thereby causing any 
+    /// bindings to this property to be re-evaluated.
+    /// </remarks>
     public event PropertyChangedEventHandler? PropertyChanged;
 
+    /// <summary> 
+    /// Triggers the <c>PropertyChanged</c> event. 
+    /// </summary> 
+    /// <param name="propertyName">The name of the property that changed.</param>
+    /// <remarks> 
+    /// If the <c>PropertyChanged</c> event has subscribers, then this method invokes the event with 
+    /// a new instance of <c>PropertyChangedEventArgs</c> containing the name of the property that changed.
+    /// </remarks>
+    /// <example> 
+    /// This method is usually called by properties in the view model in a setter block like:
+    /// <code> 
+    /// public int SomeProperty
+    /// {
+    ///    get => _someProperty;
+    ///    set
+    ///    {
+    ///        _someProperty = value;
+    ///        OnPropertyChanged();
+    ///    }
+    /// }
+    /// </code> 
+    /// </example> 
     protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
     {
         this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 
+    /// <summary>
+    /// Sets the value of a field and notifies listeners that a property value has changed.
+    /// </summary>
+    /// <typeparam name="T">The type of the field.</typeparam>
+    /// <param name="field">The field whose value needs to be set.</param>
+    /// <param name="value">The new value to be set.</param>
+    /// <param name="propertyName">The name of the property that changed. This is optional and it is set by default
+    /// to the name of the calling member, that is, the property that is getting changed.</param>
+    /// <returns><c>true</c> if the value has changed, otherwise <c>false</c>.</returns>
+    /// <remarks>
+    /// This method is for properties that raise the <c>PropertyChanged</c> event. The method uses
+    /// <c>CallerMemberName</c> attribute that allows you to obtain the method or property name
+    /// of the caller to the method, eliminating the need to explicitly supply it.
+    /// If the new value is same as the old value, the method simply returns <c>false</c>. Otherwise, it
+    /// sets the new value, triggers <c>PropertyChanged</c> event to notify listeners and returns <c>true</c>.
+    /// </remarks>
     protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
     {
-        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        if (EqualityComparer<T>.Default.Equals(field, value))
+        {
+            return false;
+        }
+
         field = value;
         this.OnPropertyChanged(propertyName);
         return true;
